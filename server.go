0ff02a8f34c3771379e8b12219eda8ba26049009
zhanghui <zhanghui@ablecloud.cn>
2017-08-18 18:12:01 +0800

    add transmitter

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package coap
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -70,31 +71,37 @@ func (r *response) Write(p []byte) (int, error) {
 }
 
 type session struct {
-	conn    net.PacketConn
-	addr    net.Addr
-	handler Handler
-	inputc  chan []byte
+	svr   *Server
+	conn  net.PacketConn
+	addr  net.Addr
+	recvc chan []byte
+
+	seq       uint16
+	pending   map[uint16]chan struct{}
+	callbacks map[string]func(s *Server, r *Response)
+
+	//outgoingMessages map[uint16]rawMessage
 }
 
-func newSession(conn net.PacketConn, addr net.Addr, h Handler) *session {
-	return new(session).init(conn, addr, h)
+func newSession(svr *Server, conn net.PacketConn, addr net.Addr) *session {
+	return new(session).init(svr, conn, addr)
 }
 
-func (s *session) init(conn net.PacketConn, addr net.Addr, h Handler) *session {
+func (s *session) init(svr *Server, conn net.PacketConn, addr net.Addr) *session {
+	s.svr = svr
 	s.conn = conn
 	s.addr = addr
-	s.handler = h
-	s.inputc = make(chan []byte, 16)
+	s.recvc = make(chan []byte, 16)
 	go s.inputing()
 	return s
 }
 
 func (s *session) close() {
-	close(s.inputc)
+	close(s.recvc)
 }
 
 func (s *session) inputing() {
-	for data := range s.inputc {
+	for data := range s.recvc {
 		m, err := parseMessage(data)
 		if err != nil {
 			log.Printf("parse message: %v", err)
@@ -122,25 +129,50 @@ func (s *session) serve(m message) {
 }
 
 func (s *session) handleMSG(m message) {
-	// 是否为响应包
-	if s.isResponse(m) {
-		s.handleResponse(m)
-	} else {
+	if m.Code == 0 {
+		// 空消息
+		return
+	}
+
+	c := m.Code >> 5
+	switch {
+	case c == 0:
+		// 请求
 		s.handleRequest(m)
+	case c >= 2 && c <= 5:
+		// 响应
+		s.handleResponse(m)
+	default:
+		// 保留
+		log.Printf("reserved code: %d.%d", c, m.Code&0x1f)
 	}
 }
 
 func (s *session) handleACK(m message) {
+	if done, ok := s.pending[m.MessageID]; ok {
+		delete(s.pending, m.MessageID)
+		close(done)
+	}
+
+	if len(m.Token) > 0 {
+		token := string(m.Token)
+		cb, ok := s.callbacks[token]
+		if ok {
+			delete(s.callbacks, token)
+			resp := &Response{
+				Ack:     true,
+				Status:  m.Code,
+				Options: m.Options,
+				Token:   m.Token,
+				Payload: m.Payload,
+			}
+			cb(s.svr, resp)
+		}
+	}
 }
 
 func (s *session) handleRST(m message) {
-}
-
-func (s *session) isResponse(m message) bool {
-	return false
-}
-
-func (s *session) handleResponse(m message) {
+	//delete(s.outgoingMessages, m.MessageID)
 }
 
 func (s *session) handleRequest(m message) {
@@ -162,14 +194,57 @@ func (s *session) handleRequest(m message) {
 		acked:       false,
 		code:        Content,
 	}
-	s.handler.ServeCOAP(resp, req)
+	s.svr.Handler.ServeCOAP(resp, req)
 	if err := s.sendResponse(resp); err != nil {
 		log.Printf("send response: %v", err)
 	}
 }
 
+func (s *session) handleResponse(m message) {
+	if len(m.Token) > 0 {
+		token := string(m.Token)
+		cb, ok := s.callbacks[token]
+		if ok {
+			delete(s.callbacks, token)
+			resp := &Response{
+				Ack:     false,
+				Status:  m.Code,
+				Options: m.Options,
+				Token:   m.Token,
+				Payload: m.Payload,
+			}
+			cb(s.svr, resp)
+		}
+	}
+}
+
+func (s *session) genToken() string {
+	return ""
+}
+
 func (s *session) genMessageID() uint16 {
 	return 1
+}
+
+func (s *session) sendRequest(req *Request, cb func(*Server, *Response)) error {
+	token := s.genToken()
+	req.Options.SetPath(req.URL.Path)
+	m := message{
+		Type:      NON,
+		Code:      req.Method,
+		MessageID: s.genMessageID(),
+		Token:     []byte(token),
+		Options:   req.Options,
+		Payload:   req.Payload,
+	}
+	if req.Confirmable {
+		m.Type = CON
+	}
+
+	s.pending[m.MessageID] = make(chan struct{})
+	s.callbacks[token] = cb
+
+	return s.send(m)
 }
 
 func (s *session) sendResponse(resp *response) error {
@@ -231,7 +306,7 @@ func ListenAndServe(network, address string, h Handler) error {
 type Server struct {
 	Handler Handler // 请求响应接口
 
-	mu       sync.Mutex
+	mu       sync.RWMutex
 	sessions map[string]*session
 }
 
@@ -252,17 +327,25 @@ func (s *Server) Serve(l net.PacketConn) error {
 		}
 		data := make([]byte, n)
 		copy(data, buf)
-		sess := s.getSession(l, addr)
-		sess.inputc <- data
+		sess := s.addSession(l, addr)
+		sess.recvc <- data
 	}
 }
 
 // SendRequest 发送COAP请求
 func (s *Server) SendRequest(req *Request, cb func(*Server, *Response)) error {
-	return nil
+	addr, err := net.ResolveUDPAddr("udp", req.URL.Host)
+	if err != nil {
+		return err
+	}
+	sess, ok := s.getSession(addr)
+	if !ok {
+		return fmt.Errorf("session(%s) not found", addr)
+	}
+	return sess.sendRequest(req, cb)
 }
 
-func (s *Server) getSession(conn net.PacketConn, addr net.Addr) *session {
+func (s *Server) addSession(conn net.PacketConn, addr net.Addr) *session {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if s.sessions == nil {
@@ -270,8 +353,15 @@ func (s *Server) getSession(conn net.PacketConn, addr net.Addr) *session {
 	}
 	sess, ok := s.sessions[addr.String()]
 	if !ok {
-		sess = newSession(conn, addr, s.Handler)
+		sess = newSession(s, conn, addr)
 		s.sessions[addr.String()] = sess
 	}
 	return sess
+}
+
+func (s *Server) getSession(addr net.Addr) (*session, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	sess, ok := s.sessions[addr.String()]
+	return sess, ok
 }
