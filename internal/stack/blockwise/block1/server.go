fc770b0ab47e79a73752cf1a3fbd34ad6a0dcb30
zhanghui <zhanghui@ablecloud.cn>
2017-09-04 14:15:47 +0800

    update block1 server

--- internal/stack/blockwise/block1/server.go
+++ internal/stack/blockwise/block1/server.go
@@ -2,61 +2,110 @@ package block1
 
 import (
 	"bytes"
-	"errors"
+	"time"
 
 	"github.com/ironzhang/coap/internal/stack/base"
 )
 
 type server struct {
 	baseLayer *base.BaseLayer
+	timeout   time.Duration
+
+	busy      bool
+	timestamp time.Time
 	buffer    bytes.Buffer
 	messageID uint16
 	block1    uint32
 }
 
-func (c *server) init(baseLayer *base.BaseLayer) {
-	c.baseLayer = baseLayer
+func (s *server) init(baseLayer *base.BaseLayer) {
+	s.baseLayer = baseLayer
 }
 
-func (c *server) recv(m base.Message) error {
-	block1Opt, ok := base.ParseBlock1Option(m)
+func (s *server) update() {
+	if s.busy && time.Since(s.timestamp) > s.timeout {
+		s.busy = false
+	}
+}
+
+func (s *server) recv(m base.Message) error {
+	if s.busy {
+		return s.busyStateRecv(m)
+	}
+	return s.idleStateRecv(m)
+}
+
+func (s *server) send(m base.Message) error {
+	if !s.busy {
+		return s.baseLayer.Send(m)
+	}
+	if m.MessageID != s.messageID {
+		return s.baseLayer.Send(m)
+	}
+	s.busy = false
+	m.SetOption(base.Block1, s.block1)
+	return s.baseLayer.Send(m)
+}
+
+func (s *server) busyStateRecv(m base.Message) error {
+	opt, ok := base.ParseBlock1Option(m)
 	if !ok {
-		return c.baseLayer.Recv(m)
+		return l.baseLayer.NewError(base.ErrNoBlock1Option)
 	}
-	if c.buffer.Len() != int(block1Opt.Num*block1Opt.Size) {
-		return errors.New("request entity incomplete")
+	if s.buffer.Len() != int(opt.Num*opt.Size) {
+		return s.ackRequestEntityIncomplete(m.MessageID)
 	}
-	c.buffer.Write(m.Payload)
-	if block1Opt.More {
-		return c.ackContinue(m.MessageID, block1Opt.Value())
+	s.buffer.Write(m.Payload)
+	if opt.More {
+		return s.ackContinue(m.MessageID, opt.Value())
 	}
-	c.messageID = m.MessageID
-	c.block1 = block1Opt.Value()
-	m.Payload = c.copyAndResetBuffer()
-	return c.baseLayer.Recv(m)
+	s.messageID = m.MessageID
+	s.block1 = opt.Value()
+	m.Payload = s.copyAndResetBuffer()
+	return s.baseLayer.Recv(m)
 }
 
-func (c *server) send(m base.Message) error {
-	if m.MessageID != c.messageID {
-		return c.baseLayer.Send(m)
+func (s *server) idleStateRecv(m base.Message) error {
+	opt, ok := base.ParseBlock1Option(m)
+	if !ok {
+		return l.baseLayer.Recv(m)
 	}
-	m.SetOption(base.Block1, c.block1)
-	return c.baseLayer.Send(m)
+
+	s.busy = true
+	s.timestamp = time.Now()
+	s.buffer.Reset()
+	s.buffer.Write(m.Payload)
+	if opt.More {
+		return s.ackContinue(m.MessageID, opt.Value())
+	}
+	s.messageID = m.MessageID
+	s.block1 = opt.Value()
+	m.Payload = s.copyAndResetBuffer()
+	return s.baseLayer.Recv(m)
 }
 
-func (c *server) ackContinue(messageID uint16, block1 uint32) error {
+func (s *server) ackContinue(messageID uint16, block1 uint32) error {
 	m := base.Message{
 		Type:      base.ACK,
 		Code:      base.Continue,
 		MessageID: messageID,
 	}
 	m.SetOption(base.Block1, block1)
-	return c.baseLayer.Send(m)
+	return s.baseLayer.Send(m)
 }
 
-func (c *server) copyAndResetBuffer() []byte {
-	b := make([]byte, c.buffer.Len())
-	copy(b, c.buffer.Bytes())
-	c.buffer.Reset()
+func (s *server) ackRequestEntityIncomplete(messageID uint16) error {
+	m := base.Message{
+		Type:      base.ACK,
+		Code:      base.RequestEntityIncomplete,
+		MessageID: messageID,
+	}
+	return s.baseLayer.Send(m)
+}
+
+func (s *server) copyAndResetBuffer() []byte {
+	b := make([]byte, s.buffer.Len())
+	copy(b, s.buffer.Bytes())
+	s.buffer.Reset()
 	return b
 }
