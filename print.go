df0c53869bafb2c0c6f7536a8fa77f4c9c21d135
ironzhang <zhanghui@ablecloud.cn>
2017-12-10 14:55:40 +0800

    fix print bug

--- print.go
+++ print.go
@@ -14,7 +14,7 @@ func PrintRequest(w io.Writer, r *Request, body bool) {
 }
 
 func PrintResponse(w io.Writer, r *Response, body bool) {
-	fmt.Fprintf(w, "ACK[%t] %s", r.Ack, r.Status)
+	fmt.Fprintf(w, "ACK[%t] %s\n", r.Ack, r.Status)
 	r.Options.Write(w)
 	if body {
 		fmt.Fprintf(w, "\n%s\n", r.Payload)
