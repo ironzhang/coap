c533ee08102b4e5083ea916528ed35c7810110ed
ironzhang <zhanghui@ablecloud.cn>
2017-12-10 17:07:51 +0800

    update scripts

--- tools/scripts/TestBlock.sh
+++ tools/scripts/TestBlock.sh
@@ -2,6 +2,6 @@
 
 CURL=../coap-curl/coap-curl
 
-$CURL -X POST --in-file ietf-block.html --out-file output.html coap://localhost/TestBlock
+$CURL -X POST --con --in-file ietf-block.html --out-file output.html coap://localhost/TestBlock
 
 md5sum ietf-block.html output.html
